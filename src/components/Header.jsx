d1f2498e762fb6f5e5ee65df9e23c432e0e29324
Abir Hossen <abirhossen16@gmail.com>
2025-12-12 10:54:12 +0600

    finished the cart,  added the provider to app.js used cartContext in Header and MealItems

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,13 @@
+import { useContext } from "react";
 import logoImg from "../assets/logo.jpg";
 import Button from "./UI/Button";
+import CartContext from "../store/CartContext";
 export default function Header() {
+  const cartCtx = useContext(CartContext);
+
+  const totalCartItems = cartCtx.itexm.reduce((totalNumberOfItems, item) => {
+    return totalNumberOfItems + item.quantity;
+  }, 0);
   return (
     <header id="main-header">
       <div id="title">
